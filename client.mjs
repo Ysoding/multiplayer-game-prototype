1a490780a67f4cca71dbd7d42d3a3e85f605c325
ultraman <1394466835@qq.com>
2024-09-19 21:34:05 +0800

    feat: render self

--- client.mjs
+++ client.mjs
@@ -1,16 +1,81 @@
+const WORLD_WIDTH = 800;
+const WORLD_HEIGHT = 600;
+const PLAYER_SIZE = 30;
 (async () => {
-    const ws = new WebSocket("ws://localhost:6970");
+    const gameCanvas = document.getElementById("game");
+    if (gameCanvas == null)
+        throw new Error("no element with id `game`");
+    gameCanvas.width = WORLD_WIDTH;
+    gameCanvas.height = WORLD_HEIGHT;
+    const ctx = gameCanvas.getContext("2d");
+    if (ctx == null)
+        throw new Error("2d canvas is not supported");
+    let ws = new WebSocket(`ws://${window.location.hostname}:6970`);
+    let players = new Map();
+    let me = undefined;
     ws.addEventListener("close", (event) => {
-        console.log(event);
+        console.log("WEBSOCKET CLOSE", event);
+        ws = undefined;
     });
     ws.addEventListener("error", (event) => {
-        console.log(event);
+        // TODO: reconnect on errors
+        console.log("WEBSOCKET ERROR", event);
     });
     ws.addEventListener("message", (event) => {
-        console.log(event);
+        console.log("Received message", event);
+        if (me === undefined) {
+            // init
+            // TODO: check data is correct format
+            const h = JSON.parse(event.data);
+            me = {
+                id: h.id,
+                x: h.x,
+                y: h.y,
+                hue: h.hue,
+            };
+            players.set(me.id, me);
+        }
     });
     ws.addEventListener("open", (event) => {
-        console.log(event);
+        console.log("WEBSOCKET OPEN", event);
+    });
+    let previousTimestamp = 0;
+    const frame = (timestamp) => {
+        const deltaTime = (timestamp - previousTimestamp) / 1000;
+        previousTimestamp = timestamp;
+        ctx.fillStyle = "#202020";
+        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        if (ws === undefined) {
+            console.log("disconnect");
+            const label = "Disconnected";
+            const textSize = ctx.measureText(label);
+            ctx.font = "48px bold";
+            ctx.fillStyle = "white";
+            ctx.fillText(label, ctx.canvas.width / 2 - textSize.width / 2, ctx.canvas.height / 2);
+        }
+        else {
+            players.forEach((player) => {
+                if (me === undefined || me.id == player.id) {
+                    return;
+                }
+                ctx.fillStyle = `hsl(${player.hue} 70% 40%)`;
+                ctx.fillRect(player.x, player.y, PLAYER_SIZE, PLAYER_SIZE);
+            });
+            if (me !== undefined) {
+                ctx.fillStyle = `hsl(${me.hue} 100% 40%)`;
+                ctx.fillRect(me.x, me.y, PLAYER_SIZE, PLAYER_SIZE);
+                ctx.strokeStyle = "white";
+                ctx.lineWidth = 4;
+                ctx.beginPath();
+                ctx.strokeRect(me.x, me.y, PLAYER_SIZE, PLAYER_SIZE);
+                ctx.stroke();
+            }
+        }
+        window.requestAnimationFrame(frame);
+    };
+    window.requestAnimationFrame((timestamp) => {
+        previousTimestamp = timestamp;
+        window.requestAnimationFrame(frame);
     });
 })();
 export {};
--- server.go
+++ server.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -15,7 +18,20 @@ type Player struct {
 	remoteAddress string
 }
 
-var players map[int]Player
+type HelloMsg struct {
+	X   int     `json:"x"`
+	Y   int     `json:"y"`
+	Hue float64 `json:"hue"`
+	ID  int     `json:"id"`
+}
+
+const (
+	worldWidth  = 800
+	worldHeight = 600
+	playerSize  = 30
+)
+
+var players map[int]*Player
 var idCounter = 0
 
 var upgrader = websocket.Upgrader{
@@ -24,11 +40,15 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func main() {
-	http.HandleFunc("/ws", handler)
+func init() {
+	players = make(map[int]*Player)
+}
 
-	log.Println("Listening to ws://0.0.0.0:6969")
-	if err := http.ListenAndServe(":6969", nil); err != nil {
+func main() {
+	http.HandleFunc("/", handler)
+
+	log.Println("Listening to ws://0.0.0.0:6970")
+	if err := http.ListenAndServe(":6970", nil); err != nil {
 		panic(err)
 	}
 }
@@ -41,15 +61,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
+	id := getNewID()
+	remoteAddr := conn.UnderlyingConn().RemoteAddr().String()
+
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	x := rnd.Intn(worldWidth - playerSize)
+	y := rnd.Intn(worldHeight - playerSize)
+	hue := math.Floor(rnd.Float64() * 360)
+
+	player := NewPlayer(conn, remoteAddr, id, x, y)
+	players[id] = player
+	log.Printf("Player%d connected", id)
+
+	helloMsg := HelloMsg{X: x, Y: y, ID: id, Hue: hue}
+	if err := conn.WriteJSON(helloMsg); err != nil {
+		log.Printf("Player%d send HelloMsg error:%v", id, err)
+		return
+	}
+
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
-			continue
+			log.Printf("Player%d ReadMessage error:%v", id, err)
+			break
 		}
 
 		go handleMsg(conn, messageType, p)
-
 	}
 }
 
@@ -59,5 +96,22 @@ func handleMsg(conn *websocket.Conn, messageType int, msg []byte) {
 	if err := conn.WriteMessage(messageType, msg); err != nil {
 		log.Println(err)
 		return
+	}
+
+}
+
+func getNewID() int {
+	id := idCounter
+	idCounter += 1
+	return id
+}
+
+func NewPlayer(conn *websocket.Conn, remoteAddr string, id, x, y int) *Player {
+	return &Player{
+		id:            id,
+		x:             x,
+		y:             y,
+		conn:          conn,
+		remoteAddress: remoteAddr,
 	}
 }
