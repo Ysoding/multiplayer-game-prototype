ade1ca3e0e88ae0746dc6a830d77cb4dde3bb6dd
ultraman <1394466835@qq.com>
2024-09-21 15:27:34 +0800

    chore: update

--- message/msg_test.go
+++ message/msg_test.go
@@ -1,26 +1,19 @@
 package message
 
-import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-	"testing"
-)
+// func TestEncode(t *testing.T) {
+// 	p := PlayersJoinedMsgStruct{
+// 		player: Player{
+// 			ID:  0,
+// 			X:   1.4,
+// 			Y:   1.2,
+// 			Hue: 33,
+// 		},
+// 	}
 
-func TestEncode(t *testing.T) {
-	p := PlayersJoinedMsgStruct{
-		player: player{
-			id:  0,
-			x:   1.4,
-			y:   1.2,
-			hue: 33,
-		},
-	}
-
-	buffer := new(bytes.Buffer)
-	err := binary.Write(buffer, binary.LittleEndian, p)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(buffer)
-}
+// 	buffer := new(bytes.Buffer)
+// 	err := binary.Write(buffer, binary.LittleEndian, p)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(buffer)
+// }
